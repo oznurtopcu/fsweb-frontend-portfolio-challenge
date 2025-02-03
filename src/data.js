1bbbd78310769df511d1bb931e707ed229fb9e38
oznurtopcu <topcuoznur121@gmail.com>
2025-02-03 13:57:58 +0300

    add info to data.js

--- src/data.js
+++ src/data.js
@@ -1,9 +1,114 @@
 const data = {
     en: {
-        heroSection: {},
-        skillsSection: {},
-        profileSection: {},
-        projectsSection: {},
-        footerSection: {},
+        heroSection: {
+            changeLanguage: ['TÜRKÇE',"'YE GEÇ"],
+            name: 'Öznur',
+            myTitle: 'I am a Full - Stack Developer...',
+            myTitleDesc: '...who likes to craft solid and scalable frontend products with great user experiences.',
+            social: [
+                {
+                    name:'Github',
+                    icon:'',
+                },
+                {
+                    name:'LinkedIn',
+                    icon:'',
+                }
+            ],
+        },
+        skillsSection: {
+            title: 'Skills',
+            skills: [ 
+                {
+                    name: 'JavaScript',
+                    icon: '',
+                },
+                {
+                    name: 'HTML',
+                    icon: '',
+                },
+                {
+                    name: 'CSS',
+                    icon: '',
+                },
+                {
+                    name: 'React',
+                    icon: '',
+                },
+                {
+                    name: 'Redux',
+                    icon: '',
+                },
+                {
+                    name: 'VS Code',
+                    icon: '',
+                },
+                {
+                    name: 'Figma',
+                    icon: '',
+                },
+                
+            ],
+        },
+        profileSection: {
+            title: 'Profile',
+            information: {
+                title: 'Basic Information',
+                info: [
+                    {
+                        info: 'Birth',
+                        text: '01.11.1999',
+                    },
+                    {
+                        info: 'City',
+                        text: 'Ankara',
+                    },
+                    {
+                        info: 'Education',
+                        text: 'Gazi Ünv. Elektrik Elektronik Müh. Lisans, 2022',
+                    },
+                    {
+                        info: 'Roles',
+                        text: 'Frontend',
+                    },
+                ],
+            },
+            about: {
+                title: 'About Me',
+                paragraph: 'I am a Junior Developer based in Ankara, with a degree in Electrical and Electronics Engineering,who has transitioned into the field of software development.I consider myself creative, eager to learn, open to development, responsible, and a finisher whenworking in teams and collaborating with other disciplines.',
+            },
+        },
+        projectsSection: [
+            {
+                title: 'Pizza Order Project',
+                image: '',
+                description: 'A simple, customizable, minimal pizza order app.',
+                technologies: ['react', 'cypress', 'vercel'],
+                links: {
+                    site: '',
+                    github: '',
+                },
+            },
+        ],
+        footerSection: { 
+            sendMe: 'Send me a message!',
+            text: 'Got a question for proposal, or just want to say hello? Go ahead.',
+            mail: 'topcuoznur121@gmail.com',
+            icons: [
+                {
+                    alt_text: 'LinkedIn',
+                    logo: '',
+                },
+                {
+                    alt_text: 'Github',
+                    logo: '',
+                },
+                {
+                    alt_text: 'Gmail',
+                    logo: '',
+                }
+            ],
+
+        },
     }
 }
