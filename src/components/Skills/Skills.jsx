da6ea79403d61fd4f4b953e2493599919055b60a
oznurtopcu <topcuoznur121@gmail.com>
2025-02-03 21:26:35 +0300

    create skills.jsx

---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,19 @@
+export default function Skills() {
+    return(
+        <div className="skills-section flex flex-row ">
+            
+            <div>
+                <div>
+                    <p className="text-bold text-3xl text-[#3730A3]">Skills</p>
+                </div>
+                <div className="skills-list">
+                    <div>
+                        <img src="" alt="" />
+                        <p>JavaScript</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+}
