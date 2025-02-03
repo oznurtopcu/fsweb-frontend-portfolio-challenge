bd8e28c669a06d22d557d57bd0d39075b6380726
oznurtopcu <topcuoznur121@gmail.com>
2025-02-03 14:33:07 +0300

    create hero.jsx

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,26 @@
+export default function Hero () {
+    return(
+        <div className="hero-section">
+            <header>
+                <p>Öznur</p>
+                <div>
+                    <p>Türkçe'ye Geç</p>
+                    <p>Dark Mode</p>
+                </div>
+            </header>
+            <div className="hero-content">
+                <div className="hero-info">
+                    <p>I am a frontend-developer</p>
+                    <p>...who likes to craft solid and scalable frontend products with great user experiences.</p>
+                    <div>
+                        <a href="#">Github</a>
+                        <a href="#">LinkedIn</a>
+                    </div>
+                </div>
+                <img src="" alt="profile-image" />
+            </div>
+
+        
+        </div>
+    );
+}
