9f9fbc7771196eb199ea20f6dea724c022abfb51
oznurtopcu <topcuoznur121@gmail.com>
2025-02-05 17:51:38 +0300

    delete useLocalStorage descriptions

--- src/hooks/useLocalStorage.jsx
+++ src/hooks/useLocalStorage.jsx
@@ -2,23 +2,19 @@ import { useState } from 'react';
 
 export default function useLocalStorage(key, defaultValue) {
   const [value, setValue] = useState(() => {
-    // ilk açılışta localstorage okuyoruz
+
     const localVal = JSON.parse(localStorage.getItem(key));
     if (localVal === null) {
-      // boşsa default değeri, localStorage'a atıyoruz
-      // aynı değeri dönüyoruz
+
       localStorage.setItem(key, JSON.stringify(defaultValue));
-      //console.log('local boş', defaultValue);
+
       return defaultValue;
     } else {
-      // localstorage boş değilse
-      // defaultValue değerini dönüyoruz
-      //console.log('local dolu', localVal);
+
       return localVal;
     }
   });
-  // value değiştiğinde localstorage'a yazıyoruz
-  // ve yeni değeri dönüyoruz
+
   const setLocalStorage = (newValue) => {
     console.log(newValue);
     localStorage.setItem(key, JSON.stringify(newValue));
